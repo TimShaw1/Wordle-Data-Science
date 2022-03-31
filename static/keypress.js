1bba54a4f1fc44e33ae9e875c09ab1703ee8c362
TimShaw1 <timothyshaw0225@gmail.com>
2022-03-31 19:52:15 -0400

    Added comments

--- static/keypress.js
+++ static/keypress.js
@@ -1,14 +1,18 @@
-// get input key
-let i = 0;
-let j = 0;
+let i = 0;  //columns
+let j = 0;  //rows
 
+// referenced from https://stackoverflow.com/questions/1846599/how-to-find-out-what-character-key-is-pressed
+// get input key
 document.onkeydown = function(evt) {
     let id = j.toString().concat(",",i.toString());
     evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
 
+        // if backspace or delete are pressed, delete previous character
+        // and move back a position
         if (charCode == 8 || charCode == 46)
         {
+            // ensure we don't go out of range for i
             if (i == 4)
             {
                 if (document.getElementById(id).textContent == "_")
@@ -18,6 +22,7 @@ document.onkeydown = function(evt) {
                 document.getElementById(id).textContent = "_";
             }
             else
+            // ensure we don't go out of range for i
             if (i > 0)
             {
                 i--;
@@ -28,6 +33,7 @@ document.onkeydown = function(evt) {
 
         }
         else
+        // if we press enter, move down a row
         if (charCode == 13)
         {
             if (j < 5)
@@ -38,9 +44,10 @@ document.onkeydown = function(evt) {
         }
         else
         {
-
+            // Display character in correct box
             var charStr = String.fromCharCode(charCode);
             document.getElementById(id).textContent = charStr;
+            // ensure we don't go out of range for i
             if (i < 4)
             {
                 i++;
