1db2e79ede06d84c0dd43a916da620b1d9bba114
TimShaw1 <timothyshaw0225@gmail.com>
2022-04-01 16:12:08 -0400

    Working wordle game!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, make_response, redirect, render_template, request, session
 from flask_bootstrap import Bootstrap
 from flask_session import Session
 import keyboard
@@ -38,36 +38,64 @@ with open('valid_guesses.csv', newline='') as f:
 
     guesses = list(flat_list)
 
+# Function to split word into list of letters
+def split(word):
+    return [char for char in word]
+
 # Choose random solution and store in array
 solution = random.choice(solutions)
 solution = solution.upper()
-solution_list = solution.split()
+solution_list = split(solution)
+
+# Generate a dictionary for each letter of the alphabet
+solution_dict = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+
+# Count how many times each letter is in the word
+for letter in solution:
+    solution_dict[letter] += 1
+# Store copy of original dictionary
+temp_solution_dict = solution_dict.copy()
 
 # List to store green/yellow/gray letters
-colors = [0,0,0,0,0]
-
-solution = "GREEN"
+colors = ['gray', 'gray', 'gray', 'gray', 'gray']
+temp_colors = ['gray', 'gray', 'gray', 'gray', 'gray']
+win = ['green', 'green', 'green', 'green', 'green']
 
 
 def game(word):
-
     word = word.upper()
+    word_list = split(word)
+    print(word_list)
+    print(solution_list)
 
     if word == solution:
         print("Win")
 
-    print(solution)
-    count = 0
-    for guess_letter in word:
-        if guess_letter in solution:
-            if guess_letter == solution[count]:
-                colors[count] = 'green'
-            else:
-                colors[count] = 'yellow'
-        else:
-            colors[count] = 'gray'
+    # Check for green letters
+    for i in range(5):
+        if word_list[i] == solution_list[i]:
+            colors[i] = 'green'
+            # Decrement count
+            solution_dict[solution_list[i]] -= 1
 
-        count += 1
+    # Check for yellow letters
+    for i in range(5):
+        for j in range(5):
+            # if we haven't checked the letter already, and if it is in the word, make it yellow
+            if solution_dict[word_list[i]] > 0 and word_list[i] == solution_list[j] and colors[i] == 'gray':
+                colors[i] = 'yellow'
+                solution_dict[word_list[i]] -= 1
+    # Reset colors to gray
+    for i in range(5):
+        temp_colors[i] = colors[i]
+        colors[i] = 'gray'
+    
+    # Reset dict to original dictionary
+    solution_dict.clear()
+    solution_dict.update(temp_solution_dict)
+
+
+
 
     
 
@@ -78,6 +106,11 @@ def home():
     if request.method == "POST":
         guess = request.get_json()
         game(guess)
-        print(colors)
-
+        if guess.lower() not in guesses and guess.lower() not in solutions:
+            res = make_response({"message": "invalid"}, 200)
+        elif colors == win:
+            res = make_response({"message": "win"}, 200)
+        else:
+            res = make_response({"message": temp_colors}, 200)
+        return res
     return render_template("home.html")
--- static/keypress.js
+++ static/keypress.js
@@ -1,104 +1,124 @@
 let i = 0;  //columns
 let j = 0;  //rows
 let guess = "";
+let colors = [];
+let win = ['green', 'green', 'green', 'green', 'green'];
 
 // https://stackoverflow.com/questions/40120915/javascript-function-that-returns-true-if-a-letter
 // checks if a character is a letter
-var isAlpha = function(ch){
+var isAlpha = function (ch) {
     return /^[A-Z]$/i.test(ch);
+}
+
+function checkWin(input) {
+    for (var i = 0; i < 5; i++) {
+        if (input[i] != "green") {
+            return false;
+        }
+    }
+    return true;
 }
 
 // https://pythonise.com/series/learning-flask/flask-and-fetch-api
 function submit_message() {
 
     fetch(`${window.origin}/`, {
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify(guess),
-      cache: "no-cache",
-      headers: new Headers({
-        "content-type": "application/json"
-      })
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(guess),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
     })
-      .then(function (response) {
-        if (response.status !== 200) {
-          console.log(`Looks like there was a problem. Status code: ${response.status}`);
-          return;
-        }
-        response.json().then(function (data) {
-          console.log(data);
-        });
-      })
-      .catch(function (error) {
-        console.log("Fetch error: " + error);
-      });
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log(`Looks like there was a problem. Status code: ${response.status}`);
+                return;
+            }
+            response.json().then(function (data) {
+                if (data["message"] == "invalid") {
+                    console.log("Invalid");
+                }
+                else {
+                    console.log(data);
+                    colors = data["message"];
+                    console.log(colors);
+                    for (var k = 0; k < 5; k++) {
+                        letter_id = j.toString().concat("/", k.toString());
+                        console.log(letter_id);
+                        document.getElementById(letter_id).style.background = colors[k];
+                    }
+                    // Reset column and move down 1 row
+                    i = 0;
+                    j++;
+                    guess = "";
 
-  }
+                    // If we win, stop taking guesses
+                    if (checkWin(colors)) {
+                        j = -1;
+                        return;
+                    }
+                }
+            });
+        })
+        .catch(function (error) {
+            console.log("Fetch error: " + error);
+        });
+
+}
 
 
 // referenced from https://stackoverflow.com/questions/1846599/how-to-find-out-what-character-key-is-pressed
 // get input key
-document.onkeydown = function(evt) {
-    let id = j.toString().concat(",",i.toString());
+document.onkeydown = function (evt) {
+    let id = j.toString().concat(",", i.toString());
     evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
     var charStr = String.fromCharCode(charCode);
 
-        // if backspace or delete are pressed, delete previous character
-        // and move back a position
-        if (charCode == 8 || charCode == 46)
-        {
-            // ensure we don't go out of range for i
-            if (i == 4)
-            {
-                if (document.getElementById(id).textContent == "_")
-                {
-                    i--;
-                }
-                document.getElementById(id).textContent = "_";
-            }
-            else
-            // ensure we don't go out of range for i
-            if (i > 0)
-            {
+    // if backspace or delete are pressed, delete previous character
+    // and move back a position
+    if (charCode == 8 || charCode == 46) {
+        // ensure we don't go out of range for i
+        if (i == 4) {
+            if (document.getElementById(id).textContent == "_") {
                 i--;
             }
-            id = j.toString().concat(",",i.toString());
             document.getElementById(id).textContent = "_";
-            guess = guess.slice(0,-1);
-
         }
         else
-        // if we press enter, move down a row
-        if (charCode == 13)
-        {
-            if (j < 5 && i == 4 && guess.length == 5)
-            {
-                i = 0;
-                j++;
-                submit_message();
-                guess = "";
-            }
-        }
-        else 
-        if (isAlpha(charStr))
-        {
-            // Display character in correct box
-            document.getElementById(id).textContent = charStr;
-            if (guess.length < 5)
-            {
-                guess = guess.concat(charStr);
-            }
-            else
-            {
-                guess = guess.slice(0,-1);
-                guess = guess.concat(charStr);
-            }
-
             // ensure we don't go out of range for i
-            if (i < 4)
-            {
-                i++;
+            if (i > 0) {
+                i--;
+            }
+        id = j.toString().concat(",", i.toString());
+        document.getElementById(id).textContent = "_";
+        guess = guess.slice(0, -1);
+
+    }
+    else
+        // if we press enter, move down a row
+        if (charCode == 13) {
+            if (j < 6 && i == 4 && guess.length == 5) {
+                submit_message();
             }
         }
+        else
+            if (isAlpha(charStr)) {
+                // Display character in correct box
+                document.getElementById(id).textContent = charStr;
+                if (guess.length < 5) {
+                    guess = guess.concat(charStr);
+                }
+                else {
+                    guess = guess.slice(0, -1);
+                    guess = guess.concat(charStr);
+                }
+
+                // ensure we don't go out of range for i
+                if (i < 4) {
+                    i++;
+                }
+            }
 };
