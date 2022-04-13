69f22ed0ba35c7eb4eccbbb3fdc035758086743b
TimShaw1 <timothyshaw0225@gmail.com>
2022-04-13 12:54:48 -0400

    Fixed green letter check not working properly

--- recommend_words.py
+++ recommend_words.py
@@ -29,6 +29,7 @@ def remove_words(guesses, invalid_letters, valid_letters, letter_indices):
     for word in guesses:
         temp.append(word)
 
+    # Store letters with positive indices -- these are green positions
     green_indices = {}
     for letter in letter_indices:
         for i in range(len(letter_indices[letter])):
@@ -38,22 +39,21 @@ def remove_words(guesses, invalid_letters, valid_letters, letter_indices):
                 else:
                     green_indices[letter].append(letter_indices[letter][i])
 
-    # Green letter in wrong place -- This does not completely work, but it solves the problem 
-    # A more optimal solution is possible
+    skip = False
+
+    # Green letter in wrong place 
     for word in temp:
-        for i in range(5):
-            if word[i].upper() in green_indices:
-                temp_word = word
-            # If green letter in right place, keep word
-            # This makes an error since it keeps the word when seeing the first correct letter
-            if word[i].upper() in green_indices and i in green_indices[word[i].upper()]:
-                temp_word = ""
-                break
-                
-        if temp_word in guesses and temp_word != "":
-            if word == "bunny":
-                print("bunny")
-            guesses.remove(temp_word)
+        skip = False
+        for letter in letter_indices:
+            for index in letter_indices[letter]:
+                if not skip and index >= 0:
+                    if letter in word.upper():
+                        if word[index].upper() != letter:
+                            print(word[index].upper(), letter)
+                            guesses.remove(word)
+                            skip = True
+                            break
+
     
     temp.clear()
     for word in guesses:
@@ -67,7 +67,6 @@ def remove_words(guesses, invalid_letters, valid_letters, letter_indices):
             if word[i].upper() in letter_indices and -(i+1) in letter_indices[word[i].upper()]:
                 guesses.remove(word)
                 break
-    print(guesses)
 
 # Determine frequency of letters in all possible solutions and reccomend words based on this
 def recommend_words(guesses, valid_letters, invalid_letters, g):
@@ -89,7 +88,6 @@ def recommend_words(guesses, valid_letters, invalid_letters, g):
     for letter in seen_letters:
         sorted_letters.remove(letter)
 
-    print(sorted_letters)
 
     # List to store top 10 words
     top_10 = []
@@ -108,7 +106,6 @@ def recommend_words(guesses, valid_letters, invalid_letters, g):
                 top_10.append((word, score))
                 top_10.sort(key=lambda x: x[1], reverse=True)
                 top_10.pop()
-    print(top_10)
 
     
     return top_10
